4caec3decc03f5e33098bc251da7bd22c2f16223
Ahto Vasar <avasar@tktk.ee>
2021-12-23 15:41:45 +0200

    Wheek5

---
+++ MoreLoops3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängiat + arvuti (cpu) ja kasutaja (user);
+            //mõlemad mängiad viskavad täringuid;
+            //programm kontrollib, kumb mängija viskas rohkem
+            //mängija, kes viskab rohkem, ongi mängu võitja;
+            //*täringuid visatakse kolm korda;
+            //programm kuulutab võitjat.
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 3; i++)
+
+            {
+
+                //Arvuti vise;
+                int cpuRandom = rnd.Next(1, 7);
+                //Kasutaja vise;
+                int userRandom = rnd.Next(1, 7);
+
+                int cpuScore = 0;
+                int userScore = 0;
+
+                Console.WriteLine($"Aruti viskas {cpuRandom}. Kasutaja viskas {userRandom}");
+
+                if (cpuRandom < userRandom)
+                {
+                    Console.WriteLine("Kasutaja on mängu võitnud. Palju õnne!");
+                    userScore = userScore + 1;
+                }
+                else if (cpuRandom > userRandom)
+                {
+                    Console.WriteLine("Arvuti on mängu võitnud.");
+                    cpuScore = cpuScore + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Viik!");
+                }
+
+                Console.WriteLine($"Arvuti skoor:{cpuScore} ja kasutaja skoor:{userScore}");
+
+
+            }
+        }
+    }
+}
---
+++ OrOperator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OROperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus ja salasõna
+            //kui sisestatud kasutajatunnus on "admin" ja sisestatud salasõna "admin1234"
+            //siis konsoolis kuvatakse "Teretulemast!"
+            //muul konsoolis kuvatakse "Vale kasutajatunnus või salasõna"
+
+            //OR (või) (pipes)
+
+            // true || true --> true
+            // false || true --> true
+            // true || false --> true
+            //false || false --> false
+
+            Console.WriteLine("Sisesta kasutajatunnus:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta Salasõna:");
+            string userPassword = Console.ReadLine();
+
+            if (userName != "admin" || userPassword != "admin1234")
+            {
+                Console.WriteLine("Vale kasutajatunnus või salasõna. Proovi uuesti");
+            }
+            else
+            {
+                Console.WriteLine("Teretulemast");
+            }
+
+            // "admin1" != "admin" || "admin1234" == "admin1234" --> true || false --> true
+            //"admin" != "admin" || "admin123" != "admin1234" --> false || true --> true
+            // "admion1" != "admoin" || "admin123" != "admin1234" --> true || true --> true
+            // "admin" != "admin" || "asdmin1234" != "admin1234" --> false || false --> 
+        }
+    }
+}
